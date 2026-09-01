1992e04c32195aa5570342a7c465bc6244b05b72
YzK0741 <2353036486@qq.com>
2026-09-01 22:45:13 +0800

    fix(core): report the real application name in the Vulkan instance

--- vulkan/core/core.cpp
+++ vulkan/core/core.cpp
@@ -63,9 +63,9 @@ namespace vulkan {
     void core::init_instance() noexcept {
         VkApplicationInfo app_info{};
         app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        app_info.pApplicationName = "Hello Triangle";
+        app_info.pApplicationName = "vulkan render";
         app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        app_info.pEngineName = "No Engine";
+        app_info.pEngineName = "";
         app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         app_info.apiVersion = VK_API_VERSION_1_3;
 
